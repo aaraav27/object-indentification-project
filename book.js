8de98f82eac26579e960039ff83c2931a11ca567
aaraav27 <74003571+aaraav27@users.noreply.github.com>
2021-05-30 10:16:21 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,46 @@
+img ="";
+status = "";
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "status-detecting objects";
+}
+
+function modelLoaded(){
+    console.log("model "+"is " +"loaded");
+    status = true;
+    objectDetector.detect(img, gotresult);
+}
+
+function gotresult(error , result){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(result);
+    }
+}
+
+function preload(){
+img = loadImage('book.jpg');
+}
+
+function draw(){
+    image(img, 0, 0, 300, 300);
+    fill('black');
+    text('book', 50, 50);
+    noFill();
+    stroke('blue');
+    rect(40, 10, 200 , 300);
+
+
+   
+ 
+    
+}
+
+function goback(){
+    window.location = "index.html";
+}
